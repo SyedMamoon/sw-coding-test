f97ce6ce5bc4e51bf48d5519dd066c151adf1568
Mamoon Mehmood <mamoon.mehmood1@medznmore.com>
2023-07-28 01:05:23 +0500

    Added JsonParser.java class for reading data from JSON

---
+++ src/main/java/com/smallworld/util/JsonParser.java
@@ -0,0 +1,23 @@
+package com.smallworld.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smallworld.data.Transaction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonParser {
+
+  public static List<Transaction> getAllTransaction() {
+    ObjectMapper mapper = new ObjectMapper();
+
+    try {
+      File initialFile = new File(File_Path_Constants.TRANSACTION_JSON_FILE);
+      return mapper.readValue(initialFile, new TypeReference<List<Transaction>>() {});
+    } catch (IOException e) {
+      throw new UnsupportedOperationException("Error occurred while reading transactions :" + e);
+    }
+  }
+}
