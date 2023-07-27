869a16eb0db3313f6d115990b077a18978316aee
Mamoon Mehmood <mamoon.mehmood1@medznmore.com>
2023-07-28 00:58:36 +0500

    Created Model for Transaction.java

--- src/main/java/com/smallworld/data/Transaction.java
+++ src/main/java/com/smallworld/data/Transaction.java
@@ -1,5 +1,32 @@
 package com.smallworld.data;
 
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
 public class Transaction {
     // Represent your transaction data here.
+    private Integer mtn;
+    private Double amount;
+    private String senderFullName;
+    private Integer senderAge;
+    private String beneficiaryFullName;
+    private Integer beneficiaryAge;
+    private Integer issueId;
+    private Boolean issueSolved;
+    private String issueMessage;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return mtn.equals(that.mtn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mtn);
+    }
 }
